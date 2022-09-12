e3c702026fc96b70c14ceac32f736d66efa4910f
chase <shao4598@outlook.com>
2022-09-12 17:21:14 +0800

    首次提交

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<!--
+ * @Author: chase shao4598@outlook.com
+ * @Date: 2022-09-12
+ * @LastEditors: chase shao4598@outlook.com
+ * @LastEditTime: 2022-09-12
+ * @FilePath: /arco-design-demo/src/App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+  <a-button type="primary">button</a-button>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: chase shao4598@outlook.com
+ * @Date: 2022-09-12
+ * @LastEditors: chase shao4598@outlook.com
+ * @LastEditTime: 2022-09-12
+ * @FilePath: /arco-design-demo/src/main.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { createApp } from 'vue'
+import ArcoVue from '@arco-design/web-vue';
+import './style.css'
+import App from './App.vue'
+import '@arco-design/web-vue/dist/arco.css';
+
+const app = createApp(App);
+app.use(ArcoVue);
+app.mount('#app');
