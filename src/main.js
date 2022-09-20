401ff8148c6052ef2f7aebbd2dcde48fb6326b0e
chase <shao4598@outlook.com>
2022-09-20 09:43:45 +0800

    feat: 初始化路由

--- src/main.js
+++ src/main.js
@@ -7,11 +7,14 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { createApp } from 'vue'
+import router from './router';
 import ArcoVue from '@arco-design/web-vue';
 import './style.css'
 import App from './App.vue'
 import '@arco-design/web-vue/dist/arco.css';
 
 const app = createApp(App);
+
+app.use(router)
 app.use(ArcoVue);
 app.mount('#app');
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
+/*
+ * @Author: chase shao4598@outlook.com
+ * @Date: 2022-09-12
+ * @LastEditors: chase shao4598@outlook.com
+ * @LastEditTime: 2022-09-12
+ * @FilePath: /arco-design-demo/vite.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias:{
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
 })
